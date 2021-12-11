5f54d9b036dd96c1b6c3c420ddb6d9748e4dcf28
Eric Benedetto <ericsplace3@gmail.com>
2021-12-11 15:55:03 -0500

    optimize

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial import cKDTree
 
+# Lattice is 1 sq. km, so each tick is 1/100 km, or 10m
 popDensity = 10000
-infectionUpperBound = 0.95
-recoveryRate = 0.0002
-infectableDistance = 0.2
-stepLength = 1
-simClock = 0
 latticeWidth = 100
 latticeHeight = 100
+
 # 9 Directions for 2D Walk
 directions = [(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+
+# Simulation Parameters
+infectionUpperBound = 0.95
+recoveryRate = 0.0002
+infectableDistance = 0.2 # 6 Feet (10m * 0.2)
+stepLength = 0.15 # 5 Feet (10m * 0.15)
+
+# Counters & State
+simClock = 0
+peopleCoords = list()
+peopleCoordsX = np.zeros((popDensity))
+peopleCoordsY = np.zeros((popDensity))
+peopleLabels = np.zeros((popDensity))
+distTree = None
+casesOverTime = list()
+numberInfected = 0
 
 def walk():
     return random.choice(directions)
@@ -20,56 +34,56 @@ def generateRandomCoords():
     return (np.random.uniform(0, latticeWidth - 1), np.random.uniform(0, latticeHeight - 1))
 
 def initPopulation():
+    global numberInfected
     people = []
     for i in range(popDensity - 1):
-        (x, y) = generateRandomCoords()    
+        (x, y) = generateRandomCoords()
+        peopleCoords.append((x, y))
+        peopleCoordsX[i] = x
+        peopleCoordsY[i] = y
         people.append(Person(i, x, y))
+    
+    i += 1
+    x = latticeWidth / 2
+    y = latticeHeight / 2
 
-    infectedPerson = Person(i, latticeWidth / 2, latticeHeight / 2)
+    infectedPerson = Person(i, x, y)
     infectedPerson.infected = True
-    people.append(infectedPerson)
 
+    peopleCoords.append((x, y))
+    peopleCoordsX[i] = x
+    peopleCoordsY[i] = y
+    peopleLabels[i] = 1
+    numberInfected += 1
+
+    people.append(infectedPerson)
     return people
 
 class Person:
     def __init__(self, id, x, y):
-        self.__id = id
-        self.__x = float(x)
-        self.__y = float(y)
+        self.id = id
+        self.x = float(x)
+        self.y = float(y)
         self.immune = False
         self.infected = False
         # Five Percent Chance of Re-Infection
         self.canRelapse = True if np.random.uniform(1,20) == 1 else False
 
     def getCoords(self):
-        return (self.__x, self.__y)
+        return (self.x, self.y)
 
     def __setCoords(self, x, y):
-        self.__x = x
-        self.__y = y
+        self.x = x
+        self.y = y
+        peopleCoords[self.id] = (self.x, self.y)
+        peopleCoordsX[self.id] = self.x
+        peopleCoordsY[self.id] = self.y
 
     def __enforceBoundary(self, dx, dy):
-        dx = dx if self.__x + dx < latticeWidth else -dx
-        dy = dy if self.__y + dy < latticeHeight else -dy
+        dx = dx if self.x + dx < latticeWidth and self.x + dx > 0 else -dx
+        dy = dy if self.y + dy < latticeHeight and self.y + dy > 0 else -dy
 
         return (dx, dy)
-
-    def updateInfected(self, people):
-        # See if Anyone Will Infect Me
-        if(self.infected == False):
-            for person in people:
-                if(person.infected == True and self.withinInfectibleDistance(person) == True):
-                    self.infected = True
-                    break
-
-        # See if I Will Infect Anyone
-        if(self.infected == True):
-            for person in people:
-                if(person.infected == False and self.withinInfectibleDistance(person) == True):
-                    person.infected = True
-                    
-                    # TODO: See if Recursing Here is Necessary
-                    # person.updateInfected(people)
 
     def takeStep(self):
         (dx, dy) = walk()
@@ -79,31 +93,40 @@ class Person:
 
         (dx, dy) = self.__enforceBoundary(dx, dy)
 
-        self.__setCoords(self.__x + dx, self.__y + dy)
+        self.__setCoords(self.x + dx, self.y + dy)
 
-    def withinInfectibleDistance(self, person):
-        (otherX, otherY) = person.getCoords()
-        
-        return np.sqrt((otherX - self.__x)**2 + (otherY - self.__y)**2) <= 2
+def updateInfected(people):
+    # Look At Everyone Around Us Within Infectible Distance to See Who We Infect
+    global numberInfected
+    visited = set()
+    for idx in range(len(people)):
+        person = people[idx]
+        if person.infected is True and idx not in visited:
+            res = distTree.query_ball_point(peopleCoords[idx], r=infectableDistance)
 
-    def getId(self):
-        return self.__id
+            for neighbor in res:
+                if people[neighbor].infected is False:
+                    visited.add(neighbor)
+                    people[neighbor].infected = True
+                    numberInfected += 1
+                    peopleLabels[neighbor] = 1
 
-def renderPopulation(people):
-    peopleCoords = [person.getCoords() for person in people]
-    xs = [coords[0] for coords in peopleCoords]
-    ys = [coords[1] for coords in peopleCoords]
-    labels = [int(person.infected == True) for person in people]
+def render(infectedPercent):
+    plt.cla()
+    plt.title(f"Population at t={simClock}: {round(infectedPercent * 100, 2)}%")
+    plt.scatter(peopleCoordsX, peopleCoordsY, c=peopleLabels, s=2, cmap='bwr')
 
-    plt.title("Population")
-    plt.scatter(xs, ys, c=labels, s=2, cmap='bwr')
     plt.pause(0.05)
 
 def __main__():
     global simClock
+    global distTree
+    global numberInfected
     people = initPopulation()
+
+    distTree = cKDTree(peopleCoords, leafsize=2)
+    
     infectedPercent = 1 / popDensity
-    prevInfected = infectedPercent
 
     while(infectedPercent < infectionUpperBound):
         for i in range(len(people)):
@@ -114,17 +137,26 @@ def __main__():
 
             person.takeStep()
 
-            person.updateInfected(people)
+        updateInfected(people)
 
-            infectedPercent = [person.infected for person in people].count(True) / popDensity
-            simClock += 1
+        numberInfected = [person.infected for person in people].count(True)
+        casesOverTime.append(numberInfected)
+        
+        infectedPercent =  numberInfected / popDensity
+        simClock += 1
 
-            if(prevInfected != infectedPercent):
-                prevInfected = infectedPercent
-                renderPopulation(people)
-    
-    renderPopulation(people)
+        print(simClock)
+        if(simClock % 10 == 0):
+            render(infectedPercent)
+
+    render(infectedPercent)
+
+    plt.figure(2)
+    plt.title("Cases Over Time")
+    plt.plot(casesOverTime)
+
     plt.show()
+
 
 if __name__ == '__main__':
     __main__()
